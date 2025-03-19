1a4aaa40dd58cf0ce3b745eee7e5dd5c933456e9
854113 <9392@holbertonstudents.com>
2025-03-19 13:27:44 +0000

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name using a dunction pointer
+ * @name: name to print
+ * @f: pointer to the function that will print the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f!= NULL)
+		f(name);
+}
