e88c29f18bc2c030f24c3924b66139baf7350756
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-04-24 15:59:12 -0300

    Create dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,54 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: A dog struct
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints the details of a dog
+ * @d: A dog struct
+ */
+void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: A pointer to the new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - Frees a dog
+ * @d: A pointer to the dog to be freed
+ */
+void free_dog(dog_t *d);
+
+#endif
