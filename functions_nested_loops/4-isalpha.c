03920ea39106095baab4afcde2d2557cc07d19c6
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-06-23 23:33:03 -0300

    Update 4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - function is used to check lowercase letter
+ *
+ * @c: character check
+ *
+ * Return: c is 1 and letters is 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
