36756e40de0c9736ac5a2de7d4ffa6a1a0690a3f
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-06-23 23:32:18 -0300

    Update 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - imprime el alfabeto en minuscula x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
