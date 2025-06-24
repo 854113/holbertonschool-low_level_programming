44ca6fc964296fd5e96a66167cb7bf95f9197039
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-06-23 23:31:25 -0300

    Update 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a�ade dos integrales y retorna el resultado
+ * @number1: primer numero
+ * @number2: segundo numero
+ * Return: Resultado
+ */
+
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
