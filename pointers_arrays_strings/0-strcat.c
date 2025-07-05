41d1661ce2acbad39a8bd2dce9519b52f5a024df
854113 <9392@holbertonstudents.com>
2025-07-05 17:28:37 +0000

    update

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,34 @@
+#include "main.h"
 
+/**
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of the first unmatched characters.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	if (s1[i] == '\0' && s2[i] != '\0')
+	{
+		return (-s2[i]);
+	}
+	if (s1[i] != '\0' && s2[i] == '\0')
+	{
+		return (s1[i]);
+	}
+	return (0);
+}
