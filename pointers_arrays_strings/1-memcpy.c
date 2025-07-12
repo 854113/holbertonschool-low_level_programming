bae77265a44ad3334da93d10e21445a95a945302
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-07-12 20:53:50 -0300

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *
+ * @dest: A pointer
+ * @src: The source buffer
+ * @n: The number of bytes 
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
