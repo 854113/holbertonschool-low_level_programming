7c6fc79657103aeac40fbbae9475b05a9b7e5b6a
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-07-12 20:56:40 -0300

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,29 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * _strchr - Locates a character in a string.
+ *
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
