f6fa1f2f863b4bbca2394e9d9e54470023b98eb5
854113 <9392@holbertonstudents.com>
2025-07-26 20:01:21 +0000

    update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,1 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int r;
+	list_t *newnode;
+
+	for (r = 0; str[r] != '\0'; r++)
+	{
+	}
+	newnode = malloc(sizeof(list_t));
+		if (newnode == NULL)
+		{
+			return (NULL);
+		}
+
+	newnode->str = strdup(str);
+	newnode->len = r;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newnode);
+}
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,49 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int r;
+	list_t *newnodo;
+	list_t *end = *head;
+
+	for (r = 0; str[r] != '\0'; r++)
+	{
+	}
+	newnodo = malloc(sizeof(list_t));
+		if (newnodo == NULL)
+		{
+			return (NULL);
+		}
+
+	newnodo->str = strdup(str);
+	newnodo->len = r;
+	newnodo->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newnodo;
+	}
+	else
+	{
+		while (end->next != NULL)
+		{
+			end = end->next;
+		}
+
+		end->next = newnodo;
+	}
+	return (newnodo);
+}
