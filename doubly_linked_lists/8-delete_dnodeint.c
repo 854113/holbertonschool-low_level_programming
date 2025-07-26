157ea5458b526e32af5ee633c729cc758b34eff2
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-07-26 17:44:23 -0300

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,11 +12,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int count;
+	dlistint_t *tmp = *head;
 
-	while (*head != NULL && count != index -1)
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
 	{
-		*head = (*head)->next;
-		count++;
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
 	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
 }
